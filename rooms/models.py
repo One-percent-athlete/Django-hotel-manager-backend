4cdb22d0812aeb6c7ea177ff8dd141235e08cedc
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-26 07:34:38 +0900

    Create room and booking models

--- rooms/models.py
+++ rooms/models.py
@@ -1,5 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class RoomType(models.Model):
     title = models.CharField(max_length=20)
     details = models.JSONField(null=True)
+
+    def __str__(self):
+        return self.title
+    
+class Room(models.Model):
+    room_type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
+    room_number = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+
+class Booking(models.Model):
+    room_number = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+    total_guest = models.IntegerField()
+    checkin_date = models.DateField()
+    checkout_date = models.DateField()
+    booking_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    booking_details = models.JSONField()
