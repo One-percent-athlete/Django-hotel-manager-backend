e0596f67df0a29569ab2e22b3fcaf4f99a46f0fd
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-28 12:19:14 +0900

    Create event app

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
-User = get_user_model()
+# User = get_user_model()
 
 class RoomType(models.Model):
     title = models.CharField(max_length=20)
